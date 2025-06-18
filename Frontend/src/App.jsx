b92f4f9a69b9f4b0580fc044d1831175a346fd19
irshadhaque <irshadul.haque2508@gmail.com>
2025-06-19 01:06:14 +0530

    fixed minor bug

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
-import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
 function App() {
   return (
@@ -9,8 +8,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/login" element={<Login />}  />
         
       </Routes>
     </Router>
--- Frontend/src/components/AboutUs.jsx
+++ Frontend/src/components/AboutUs.jsx
@@ -1,48 +1,4 @@
-// import React, { useEffect, useState } from 'react';
-// import about1 from '../assets/about1.jpeg';
-// import about3 from '../assets/about3.jpeg';
-// import about4 from '../assets/about4.jpeg';
 
-// const images = [about1, about3, about4];
-
-// export default function AboutUsSection() {
-//   const [currentImage, setCurrentImage] = useState(0);
-
-//   useEffect(() => {
-//     const interval = setInterval(() => {
-//       setCurrentImage((prev) => (prev + 1) % images.length);
-//     }, 3000); // Change every 3 seconds
-//     return () => clearInterval(interval);
-//   }, []);
-
-//   return (
-//     <section className="w- py-12 px-6 md:px-20 bg-gradient-to-r from-[#0f172a] to-[#1e1b4b] text-white rounded-xl shadow-lg">
-//       <div className="w-full max-w-6xl rounded-xl shadow-lg p-6 md:p-10 flex flex-col md:flex-row items-center justify-between gap-10">
-//         <div className="flex-1">
-//           <h2 className="text-3xl md:text-4xl font-bold mb-4">About LegalCare</h2>
-//           <p className="text-gray-300 text-lg leading-relaxed mb-6">
-//             LegalCare is committed to transforming how enterprises manage legal operations.
-//             Our mission is to make legal processes seamless, accessible, and data-driven.
-//             We provide technology-driven solutions that simplify compliance, protect rights,
-//             and empower employees.
-//           </p>
-//           <button className="px-5 py-2 bg-blue-600 hover:bg-blue-500 rounded text-white font-medium transition">
-//             Read More →
-//           </button>
-//         </div>
-
-//         {/* Right Image */}
-//         <div className="flex-1">
-//           <img
-//             src={images[currentImage]}
-//             alt="About Us"
-//             className="w-full h-[300px] object-cover rounded-xl shadow-md transition duration-1000 ease-in-out"
-//           />
-//         </div>
-//       </div>
-//     </section>
-//   );
-// }
 import React, { useEffect, useState } from 'react';
 import about1 from '../assets/about1.jpeg';
 import about3 from '../assets/about3.jpeg';
